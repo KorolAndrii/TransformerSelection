f8fb631dfb734ba9b815ad910399bb10ff1774fa
KorolAndrii <jorneey@ukr.net>
2016-09-08 22:42:01 +0300

    Initial commit

---
+++ src/main/java/com/korol/TransformerSelectionApplication.java
@@ -0,0 +1,21 @@
+package com.korol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.korol")
+public class TransformerSelectionApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TransformerSelectionApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TransformerSelectionApplication.class);
+	}
+}
